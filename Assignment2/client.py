03f6fd6f9a6dfa46f35e8063f91b1c6025152348
JunHao9999 <h.junhao@yahoo.com.sg>
2022-03-18 02:02:44 +0800

    can i push?

--- Assignment2/client.py
+++ Assignment2/client.py
@@ -1,44 +1,50 @@
 from ipaddress import ip_address
-import socket,time
+import socket,time,threading
 
-PORT = 5050
-#socket.gethostbyname(scoket.gethostname())
-SERVER = "localhost"
-ADDR = (SERVER,PORT)
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "!disconnect"
 
-def connect():
-    #AF_INET ipv4 addressing, SOCK_STREAM tcp socket
-    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    client.connect(ADDR)
-    return client
 
-def send(client,msg):
-    message = msg.encode(FORMAT)
-    client.send(message)
+x, y = input('Enter chat server\'s IP address and port: ').split()
+SERVER = x
+PORT = int(y)
+ADDR = (SERVER, PORT)
+nickname = input("Choose a nickname:")
 
-def start():  
-    global PORT,SERVER,ADDR
-    print('Running client program...')
-    x,y = input('Enter chat server\'s IP address and port: ').split()
-    SERVER = x
-    PORT = int(y)
-    ADDR = (SERVER,PORT)
-    user = input('Enter this client\'s name: ')
-    print(f'Trying to connect to the server: {ADDR}')
-    connection = connect()
-    print('...Server connected...')
-    send(connection,user)
-    print('Type !disconnect to close the client program')
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def receiveClient():
     while True:
-        msg = input(f"{user}: ")
-        if msg == '!disconnect':
+        try:
+            msg = client.recv(1024).decode(FORMAT)
+            if msg=="getNickname":
+                client.send(nickname.encode(FORMAT)) #if new connection, server will ask user to send nickname
+            else:
+                print(msg)  # connection already establish. just print msg
+
+        except: #any exception = disconnect
+            print("Disconnected")
+            client.close()
             break
-        send(connection,msg)
 
-    send(connection,DISCONNECT_MESSAGE)
-    time.sleep(1)
-    print("Disconnected from chatroom")
+def write():
+    print('Running client program...')
+    print(f'Trying to connect to the server: {ADDR}')
 
-start()
+    while True:
+        userInput=input("") #get any msg wants to send across chat
+        msg = f'{nickname}:{userInput}'
+        client.send(msg.encode(FORMAT)) #send msg to server
+
+        if userInput==DISCONNECT_MESSAGE: #if user want to disconnect
+            client.close()
+            break
+
+#run both receive and write threads.
+receive_thread=threading.Thread(target=receiveClient)
+receive_thread.start()
+
+write_thread=threading.Thread(target=write)
+write_thread.start()
--- Assignment2/server.py
+++ Assignment2/server.py
@@ -1,66 +1,66 @@
 import threading,socket
 
-PORT = 5050
-#socket.gethostbyname(scoket.gethostname())
-SERVER = "172.0.0.1"
-ADDR = (SERVER,PORT)
+
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "!disconnect"
-
 #AF_INET ipv4 addressing, SOCK_STREAM tcp socket
 
+x, y = input("CSC1010 Chat server, please enter the listening IP address and port.").split()
+SERVER = x
+PORT = int(y)
+ADDR = (SERVER, PORT)
 
-clients = set()
-clients_lock = threading.Lock()
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
 
-def handle_client(conn,addr):
-    user = conn.recv(1024).decode(FORMAT)
-    print(f"{addr}:{user} established")
-    try:
-        connected = True
-        
-        while connected:
-            msg = conn.recv(1024).decode(FORMAT)
-            if not msg:
-                break
+print(f"[SERVER STARTED :{ADDR}]")
+print("Waiting for incoming connections...")
+server.listen()
 
-            if msg == DISCONNECT_MESSAGE:
-                print(f"{user} has disconnected...")
-                connected = False
+# arr to store connected clients and their nicknames
+clientsConnected = []
+nicknames = []
 
-            print(f"{user}: {msg}")
+# send message to all client connected to server
+def broadcast(message):
+    for client in clientsConnected:
+        client.send(message)
 
-            with clients_lock:
-                for c in clients:
-                    c.sendall(f"{user}: {msg}".encode(FORMAT))
-    finally:
-        with clients_lock:
-            clients.remove(conn)
-        conn.close()
+# function to handle connected user
+def handle_connection(conn):
+    while True:
+        try:
+            #if able to recieve msg, broadcast msg to all connected client in clientsConnected[]
+            msg = conn.recv(1024)
+            broadcast(msg)
+            printmsg=msg.decode(FORMAT)
+            print(printmsg)
+        except:
+            #if expection occur, remove connected client from both nicknames[] and clientsConnected[]
+            index = clientsConnected.index(conn)
+            clientsConnected.remove(conn)
+            conn.close() #close connection
+            nickname=nicknames[index] # so nickname and client have the same index
+            broadcast(f'{nickname} left the chat!'.encode(FORMAT))
+            print(f"{nickname} disconnected")
+            nicknames.remove(nickname)
+            break
 
-def start():
-    print("CSC1010 Chat server, please enter the listening IP address and port.")
-    global SERVER, PORT, ADDR
-    x,y = input().split()
-    SERVER = x
-    PORT = int(y)
-    ADDR = (SERVER,PORT)
-    
-    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    server.bind(ADDR)
 
-    print(f"[SERVER STARTED :{ADDR}]")
-    print("Waiting for incoming connections...")
-    server.listen()
-
+def receiveServer():
     while True:
         conn, addr = server.accept()
-        #Ensure one thread is being modified one at a time
-        with clients_lock:
-            clients.add(conn)
-        #Create a new thread so we can listen from more client to enusre its not blocked
-        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        print(f"Connect with {str(addr)}")
+        conn.send("getNickname".encode(FORMAT))     #getNickname to inform client.py to send nickname to server. to
+                                                    #server to identify client
+        nickname = conn.recv(1024).decode(FORMAT) #get nickname from client
+        nicknames.append(nickname)  #append to nicknames[]
+        clientsConnected.append(conn) #append to clientsConnected[]
+
+        print(f"Client is {nickname}")
+        broadcast("{} joined the chat!".format(nickname).encode(FORMAT))
+        conn.send("Connected to chat room".encode(FORMAT))
+        thread = threading.Thread(target=handle_connection, args=(conn,))
         thread.start()
 
-start()
-
+receiveServer()
