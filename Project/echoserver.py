c6ddff409511945643d32521ff531b049c21ac77
JunHao9999 <h.junhao@yahoo.com.sg>
2022-03-24 15:15:52 +0800

    test echoServer

--- Project/echoserver.py
+++ Project/echoserver.py
@@ -2,12 +2,43 @@ import socket
 import sys
 from threading import Thread
 from datetime import datetime
+import time, datetime
+import telepot
+from telepot.loop import MessageLoop
+
+telegram_bot = telepot.Bot('5116787822:AAGMxhOJb6esbb2rcAmAk-QgNm15ukXtDss')
+checkWater=False
+now = datetime.datetime.now()
+chatIdServer=[]
+waterLevel=""
+
+def action(msg): #done at msgLoop
+    global chatIdServer
+    chat_id = msg['chat']['id'] # auto get current chat id
+    command = msg['text']
+
+    print ('Received: %s' % command)
+    if command == '/start':
+        telegram_bot.sendMessage (chat_id, str("initializing start for pet water bottle"))
+        if chat_id not in chatIdServer:
+            chatIdServer.append(chat_id)
+    # elif command == '/list':
+    #     telegram_bot.sendMessage (chat_id, str("listing all chat id")+str(chatIdServer))
+    elif command == '/check':
+        telegram_bot.sendMessage(chat_id, str(now.hour)+str(":")+str(now.minute)+"waterLvl")
+        checkWater=True
 
 """def child(connectionSocket):
     data = connection.recv(16)
     print (sys.stderr, 'received "%s"' % data)
     connection.sendall(data)
     connection.close()"""
+
+def getWater(data):
+    global waterLevel
+    waterLevel = data
+    checkWater=False
+
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,6 +51,8 @@ sock.bind(server_address)
 sock.listen(1)
 
 
+MessageLoop(telegram_bot, action).run_as_thread() #run on other thread
+print('Server started for telegram')
 
 while True:
     print (sys.stderr, 'waiting for a connection')
@@ -30,7 +63,10 @@ while True:
         print (sys.stderr, 'client connected:', client_address)
         while True:
             data = connection.recv(128)
-            
+            if checkWater:
+                getWater(data)
+
+
             print (sys.stderr, 'received "%s"' % data)
             if data:
                 connection.sendall(data)
@@ -38,3 +74,10 @@ while True:
                 break
     finally:
         connection.close()
+
+
+
+
+
+
+
