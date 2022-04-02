cff55c7c2b81b06002a0ca13790f0036df5bd4c0
Insaneboy9 <93643504+Insaneboy9@users.noreply.github.com>
2022-04-02 19:43:34 +0800

    Add files via upload
    MOST UPDATED

--- Assignment2/client.py
+++ Assignment2/client.py
@@ -1,63 +1,73 @@
+from fileinput import filename
 from ipaddress import ip_address
-import socket,time,threading
+import socket, time, threading
+import os
 
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "!disconnect"
 
+x, y = input('Enter chat server\'s IP address and port: ').split()  # take users input for ip address and port number
+SERVER = x  # set server to users choice of ip address
+PORT = int(y)  # set port to users choice of port
+ADDR = (SERVER, PORT)  # set address to server and port
+nickname = input("Choose a nickname:")  # take users choice of name
 
-x, y = input('Enter chat server\'s IP address and port: ').split()
-SERVER = x
-PORT = int(y)
-ADDR = (SERVER, PORT)
-nickname = input("Choose a nickname:")
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # initialize client with socket object
+client.connect(ADDR)  # connect client and address
 
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client.connect(ADDR)
+filename = ""
 
 
 def receiveClient():
-    condition=True
-    while condition:
+    global filename
+    condition = True
+    while condition:  # infinite loop
         try:
-            msg1 = client.recv(1024)
-            msg=msg1.decode(FORMAT)
-            if msg=="getNickname":
-                client.send(nickname.encode(FORMAT)) #if new connection, server will ask user to send nickname
-            if msg=="toDownload":
-                pass
-            else:
-                print(msg)  # connection already establish. just print msg
+            msg1 = client.recv(4096)  # receive the message
+            if msg1.decode(FORMAT) == "getNickname":
+                client.send(nickname.encode(FORMAT))  # if new connection, server will ask user to send nickname
 
-        except: #any exception = disconnect
+            elif '!image' in msg1.decode(FORMAT):
+                image_chunk = client.recv(2048)
+                file1 = open(filename, "wb")
+                while image_chunk:
+                    file1.write(image_chunk)  #
+                    image_chunk = client.recv(2048)
+                file1.close()
+
+            else:
+                print(msg1.decode(FORMAT))  # connection already establish. just print msg
+
+        except:  # any exception = disconnect
             print("Disconnected")
             client.close()
             break
-    f.close()
+    # f.close()
 
 
 def write():
+    global filename
     print('Running client program...')
     print(f'Trying to connect to the server: {ADDR}')
-    condition=True
+    condition = True
     while condition:
-        userInput=input("") #get any msg wants to send across chat
+        userInput = input("")  # get any msg wants to send across chat
         msg = f'{nickname}:{userInput}'
-        client.send(msg.encode(FORMAT)) #send msg to server
-        userInput=userInput.lower() #format string to lower case
-        if userInput==DISCONNECT_MESSAGE: #if user want to disconnect
+
+        userInput = userInput.lower()  # format string to lower case
+        if userInput == DISCONNECT_MESSAGE:  # if user want to disconnect
             client.close()
             break
-        elif userInput=="list images": #send image list to user
-            getImg="getImg"
-            client.send(getImg.encode(FORMAT))
-        elif userInput=="download":
-            toDownload="toDownload"
-            client.send(toDownload.encode(FORMAT))
+        elif "!download" in userInput:
+            filename = userInput.split(' ')[-1]  # set file name
+            print("IMAGE DOWNLOAD SUCCESSFULLY")
+        else:
+            client.send(msg.encode(FORMAT))  # send msg to server
 
 
-#run both receive and write threads.
-receive_thread=threading.Thread(target=receiveClient)
+# run both receive and write threads.
+receive_thread = threading.Thread(target=receiveClient)
 receive_thread.start()
 
-write_thread=threading.Thread(target=write)
+write_thread = threading.Thread(target=write)
 write_thread.start()
--- Assignment2/server.py
+++ Assignment2/server.py
@@ -1,14 +1,17 @@
-import threading,socket
-
+from multiprocessing.connection import Client
+import threading, socket
+import os
+import glob
 
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "!disconnect"
-#AF_INET ipv4 addressing, SOCK_STREAM tcp socket
+# AF_INET ipv4 addressing, SOCK_STREAM tcp socket
 
-x, y = input("CSC1010 Chat server, please enter the listening IP address and port.").split()
-SERVER = x
-PORT = int(y)
-ADDR = (SERVER, PORT)
+x, y = input("CSC1010 Chat server, please enter the listening IP address and port.").split() #take users input of ip address and port
+SERVER = x #set server to users choice of ip address
+PORT = int(y) #set port to users choice of port
+ADDR = (SERVER, PORT) #set address to server and port
+# nickname = input("Choose a nickname: ")
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
@@ -20,39 +23,57 @@ server.listen()
 # arr to store connected clients and their nicknames
 clientsConnected = []
 nicknames = []
+
+# images
+image_path = os.getcwd() #return string of current work directory
+image_list = [] #declare image list as an array
+for filename in glob.glob(image_path+'/*.jpg'): #assuming gif
+    image_list.append(filename.replace(image_path+"/","")) #append every image file
+
 # send message to all client connected to server
 def broadcast(message):
     for client in clientsConnected:
         client.send(message)
 
+
+def write():
+    userInput = input("")  # get any msg wants to send across chat
+    msg = f'{"Server"}:{userInput}'
+    broadcast(msg.encode(FORMAT))  # send msg to server
+    print(msg)
+
+
 # function to handle connected user
-def handle_connection(conn):
+def handle_connection(conn, addr):
     while True:
         try:
-            #if able to recieve msg, broadcast msg to all connected client in clientsConnected[]
             msg = conn.recv(1024)
-            broadcast(msg)
-            printmsg=msg.decode(FORMAT)
-            print(printmsg)
-            if printmsg=="getImg": #if userInput is getImg, send connection image list
-                conn.send("_____Image list_____\n".encode(FORMAT))
-                conn.send("pepe1.jpg\n".encode(FORMAT))
-                conn.send("pepe2.jpg\n".encode(FORMAT))
-                conn.send("___________________".encode(FORMAT))
-            if printmsg == "toDownload":
-                pass
+            if "!list" in msg.decode(FORMAT) :  # if userInput is "list images", send connection image list
+                for i in image_list:
+                    broadcast(i.encode(FORMAT))
+                    print(i)
 
+            elif "!download" in msg.decode(FORMAT):
+                conn.send('!image'.encode(FORMAT))
+                x = msg.decode(FORMAT).split(' ')
+                fileimage = open(x[-1], 'rb')
+                image_data = fileimage.read(2048)
+                while image_data:
+                    conn.send(image_data)
+                    image_data = fileimage.read(2048)
+                fileimage.close()
 
-
-
-
+            else:  # if able to receive msg and is not one of the listed conditions, broadcast msg to all connected # client in clientsConnected[]
+                broadcast(msg)
+                printmsg = msg.decode(FORMAT)
+                print(printmsg)
 
         except:
-            #if expection occur, remove connected client from both nicknames[] and clientsConnected[]
+            #if exception occurs, remove connected client from both nicknames[] and clientsConnected[]
             index = clientsConnected.index(conn)
             clientsConnected.remove(conn)
-            conn.close() #close connection
-            nickname=nicknames[index] # so nickname and client have the same index
+            conn.close()  # close connection
+            nickname = nicknames[index]  # so nickname and client have the same index
             broadcast(f'{nickname} left the chat!'.encode(FORMAT))
             print(f"{nickname} disconnected")
             nicknames.remove(nickname)
@@ -63,15 +84,25 @@ def receiveServer():
     while True:
         conn, addr = server.accept()
         print(f"Connect with {str(addr)}")
-        conn.send("getNickname".encode(FORMAT))     #getNickname to inform client.py to send nickname to server. to
-                                                    #server to identify client
-        nickname = conn.recv(1024).decode(FORMAT) #get nickname from client
-        nicknames.append(nickname)  #append to nicknames[]
-        clientsConnected.append(conn) #append to clientsConnected[]
-        print(f"Client is {nickname}")
-        broadcast("{} joined the chat!".format(nickname).encode(FORMAT))
+        conn.send("getNickname".encode(FORMAT))  # getNickname to inform client.py to send nickname to server. to
+        # server to identify client
+        nickname = conn.recv(1024).decode(FORMAT)  # get nickname from client
+        nicknames.append(nickname)  # append to nicknames[]
+        clientsConnected.append(conn)  # append to clientsConnected[]
+        print(f"Client {nickname} has joined")
+        broadcast("{} joined the chat!\n".format(nickname).encode(FORMAT))
         conn.send("Connected to chat room".encode(FORMAT))
-        thread = threading.Thread(target=handle_connection, args=(conn,))
+        conn.send("\n---------------------------------------------------------------------".encode(FORMAT))
+        conn.send(("\nServer Functions: Type '!list' to list images in the server".encode(FORMAT)))
+        conn.send("\nType '!download <filename>' to bring up the download interface".encode(FORMAT))
+        conn.send("\n---------------------------------------------------------------------".encode(FORMAT))
+
+        thread = threading.Thread(target=handle_connection, args=(conn, addr))
         thread.start()
+        write_thread = threading.Thread(target=write)
+        write_thread.start()
+
+
 
 receiveServer()
+# handle_client()
